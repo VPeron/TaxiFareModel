0d38e969f92eeabfbe66e3d05fe7d7263f5b86d4
Vinicius Peron <viperon.python@gmail.com>
2021-10-20 15:29:37 +0200

    completed all steps

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -9,8 +9,18 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 
+# mlflow
+import mlflow
+from mlflow.tracking import MlflowClient
+
+#
+from memoized_property import memoized_property
+
 
 class Trainer():
+    
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+    EXPERIMENT_NAME = "[DE][Berlin][VPeron] TaxiFareModel v_1.0"
     
     def __init__(self, X, y):
         """
@@ -56,6 +66,29 @@ class Trainer():
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
         return rmse
+    
+    
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.EXPERIMENT_NAME)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.EXPERIMENT_NAME).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
@@ -64,11 +97,21 @@ if __name__ == "__main__":
     df = clean_data(df)
     y = df["fare_amount"]
     X = df.drop("fare_amount", axis=1)
-    from sklearn.model_selection import train_test_split
     
-
+    from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
     trainer = Trainer(X_train, y_train)
     trainer.run()
+    
     rmse = trainer.evaluate(X_test, y_test)
     print(f"rmse: {round(rmse, 2)}")
+    
+    # trainer.mlflow_run()  # how to init the run? -> currently no errors are thrown but the run is not logged.
+    trainer.mlflow_log_param("linear_model", LinearRegression)  
+    trainer.mlflow_log_metric("rmse", rmse)
+    
+    # retrieve id for easy access in mlflow API
+    experiment_id = trainer.mlflow_experiment_id
+    print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
+
+
